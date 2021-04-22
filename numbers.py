ce69a9bb0f0f1d655ae6947209563345205710d3
KuntalGhosal <kuntalg10@gmail.com>
2021-04-23 02:32:13 +0530

    basic string list function

---
+++ numbers.py
@@ -0,0 +1,54 @@
+#
+
+# my_income = 300000
+# my_taxrate= 0.1
+# my_tax = my_income * my_taxrate
+
+# print(my_tax)
+
+
+# myString = "Kuntal Ghosal"
+
+# print(myString [::2])
+# x = myString.split()
+# print(x)
+
+# x = "National Animal: {} National Flower: {}".format("Tiger", "Lotus")
+
+# x = "National Animal: {a} National Flower: {b}".format(a="Tiger", b="Lotus")
+# print(x)
+
+
+# Python List
+
+
+# myList = ["avsvv", 1, 2, 3, 4, 5, 6]
+
+# listTwo = [44, 55, 22, 11]
+
+# myList.extend(listTwo)
+
+# print(len(myList))
+# print(myList[0])
+# print(myList[1:4])
+# print(myList[::2])
+
+# This is replace the item with his given position
+
+# myList[1] = "Kuntal"
+
+# append add item from end
+
+# myList.append("Hello")
+# print(myList)
+
+# Pop function by default delete the item from the last and also delete the item of given index number
+
+# a = [1, 2, 3, 6, 5, 4]
+# b = ["a", "g", "f", "y", " f"]
+
+# a.pop(2)
+# a.pop()
+# a.reverse()
+# a.sort()
+# print(b)
